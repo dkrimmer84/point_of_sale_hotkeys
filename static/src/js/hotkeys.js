27a47d619fbbdc2ef8dd8efdfa05cec9a986b4a6
johnsh <sebastian80_23@hotmail.com>
2016-12-19 18:27:13 -0500

    Configuraciones

---
+++ static/src/js/hotkeys.js
@@ -0,0 +1,20 @@
+odoo.define('hotkeys', function (require) {
+	"use strict";
+
+	// Se importan los modulos necesarios
+	var core = require('web.core');
+	var models = require('point_of_sale.models');
+	var session = require('web.session');
+	var PosDB = require('point_of_sale.DB');
+	var Gui = require('point_of_sale.gui');
+	var chrome = require('point_of_sale.chrome');
+	var ajax = require('web.ajax');
+	var Model = require('web.Model');
+	var data = require('web.data');
+	var PosBaseWidget = require('point_of_sale.BaseWidget');
+	var Screens = require('point_of_sale.screens');
+
+	
+	console.log('testtttttt');
+
+ });
